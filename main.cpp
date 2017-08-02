2505608b8b4bfc560f14b4764583fb549c1a227a
svihra <psvihra@gmail.com>
2017-08-02 02:24:45 +0200

    updated functions and output

--- main.cpp
+++ main.cpp
@@ -24,24 +24,30 @@ const static uint m_nIterationsEmpty = 1;
 const static double m_eps_0    = 8.854e-12;
 const static double m_boltzman = 1.38064852e-23;    // m2 kg s-2 K-1
 const static double m_charge   = 1.602e-19;         // C
-const static double m_magB[3]  = {5,-5,0};           // B
+const static double m_magB[3]  = {5,-5,0};          // B
+const static double m_magBSize = sqrt(pow(m_magB[0],2)+pow(m_magB[1],2)+pow(m_magB[2],2));
 
-const static double m_elMass = 9.1094e-31;          // kg
-const static double m_elMC   = -1.758820024e11;     // C/kg
-const static double m_elTemp = 1e7;                 // K 1e7
-const static double m_elVel  = sqrt((m_boltzman*m_elTemp)/(m_multStats*m_elMass));//1 // m s-1
+const static double m_elMass  = 9.1094e-31;          // kg
+const static double m_elMC    = -1.758820024e11;     // C/kg
+const static double m_elTemp  = 1e7;                 // K 1e7
+const static double m_elVel   = sqrt((m_boltzman*m_elTemp)/(m_multStats*m_elMass));//1 // m s-1
+const static double m_elOmega = m_magBSize * m_elMC  / (2.0 * M_PI);
+const static double m_elLarm  = m_elVel / m_elOmega;
 
-const static double m_ionMass = 1.6726e-27;         // kg
-const static double m_ionMC   = 9.57883322e7;       // C/kg
-const static double m_ionTemp = 1e7;                // K
-const static double m_ionVel  = sqrt((m_boltzman*m_ionTemp)/(m_multStats*m_ionMass));//7e-2 // m s-1
+const static double m_ionMass  = 1.6726e-27;         // kg
+const static double m_ionMC    = 9.57883322e7;       // C/kg
+const static double m_ionTemp  = 1e7;                // K
+const static double m_ionVel   = sqrt((m_boltzman*m_ionTemp)/(m_multStats*m_ionMass));//7e-2 // m s-1
+const static double m_ionOmega = m_magBSize * m_ionMC  / (2.0 * M_PI);
+const static double m_ionLarm  = m_ionVel / m_ionOmega;
 
 // used scales
 const static int m_sizeX = 300;
 const static int m_sizeY = 300;
 const static double dx = m_widthX/m_sizeX;
 const static double dy = m_widthY/m_sizeY;
-const static double dt = dx/m_elVel;
+
+const static double dt = (dx/m_elVel) < (-0.2/m_elOmega) ? (dx/m_elVel) : (-0.2/m_elOmega);   // dt is either one spatial step or fifth of cyclotron frequency
 
 // used geometry
 bool m_geometry[m_sizeX][m_sizeY];
@@ -115,6 +121,34 @@ void InitGeometry()
     }
 
     cout << "Geometry initialized." << endl;
+}
+
+//
+// Initialize magnetic field
+void InitMagField(t_magField* magFieldConst)
+{
+    double magBMagnitudeSquared = VectorPow((double*) m_magB, 1);
+
+    // tan for mag field
+    if (magBMagnitudeSquared != 0)
+    {
+        magFieldConst->tan[0] = m_magB[0] * tan(m_elMC * magBMagnitudeSquared * dt / 2.0) / magBMagnitudeSquared;
+        magFieldConst->tan[1] = m_magB[1] * tan(m_elMC * magBMagnitudeSquared * dt / 2.0) / magBMagnitudeSquared;
+        magFieldConst->tan[2] = 0;
+    }
+    else
+    {
+        magFieldConst->tan[0] = 0;
+        magFieldConst->tan[1] = 0;
+        magFieldConst->tan[2] = 0;
+    }
+
+    double magTanElMagnitudeSquared = VectorPow(magFieldConst->tan,1);
+
+    // sin for mag field
+    magFieldConst->sin[0] = 2 * magFieldConst->tan[0] / (1 + magTanElMagnitudeSquared);
+    magFieldConst->sin[1] = 2 * magFieldConst->tan[1] / (1 + magTanElMagnitudeSquared);
+    magFieldConst->sin[2] = 0;
 }
 
 //
@@ -270,6 +304,8 @@ void ChargeToGrid(deque<t_particle > *particles, double rho_p, double rho[m_size
     cout << "Charge calculated to the grid." << endl;
 }
 
+//
+// Save particles to a file
 void SaveParticles(ofstream *file, deque<t_particle > *particles, int charge)
 {
     uint n_particles = particles->size();
@@ -281,6 +317,8 @@ void SaveParticles(ofstream *file, deque<t_particle > *particles, int charge)
     cout << "Particles position and velocities saved to a file." << endl;
 }
 
+//
+// Save particles to a file according to iteration number
 int SaveParticles(uint iter, deque<t_particle > *particles, int charge, string name)
 {
     ofstream file;
@@ -305,6 +343,34 @@ int SaveParticles(uint iter, deque<t_particle > *particles, int charge, string n
 
     cout << "Particles position and velocities run " << iter << " saved to a file." << endl;
     return 0;
+}
+
+//
+// Generate particles
+void GenerateParticles(deque<t_particle > *particles, double vel)
+{
+    t_particle particle;
+
+    double U1, U2, U3, U4;
+    for (uint iter = 0; iter < m_nMacroParticles; iter++)
+    {
+        // position
+        particle.pos[0] = m_widthX*((double) rand())/(RAND_MAX);
+        particle.pos[1] = m_widthY*((double) rand())/(RAND_MAX);
+        particle.node[0] = (int) (particle.pos[0]/dx);
+        particle.node[1] = (int) (particle.pos[1]/dy);
+
+        // velocity
+        U1 = ((double) rand())/RAND_MAX;
+        U2 = ((double) rand())/RAND_MAX;
+        U3 = ((double) rand())/RAND_MAX;
+        U4 = ((double) rand())/RAND_MAX;
+        particle.vel[0] = ((vel/sqrt(3))*sqrt(-2 * log(U1)) * cos(2 * M_PI * U2));
+        particle.vel[1] = ((vel/sqrt(3))*sqrt(-2 * log(U1)) * sin(2 * M_PI * U2));
+        particle.vel[2] = ((vel/sqrt(3))*sqrt(-2 * log(U3)) * cos(2 * M_PI * U4));
+
+        particles->push_back(particle);
+    }
 }
 
 //
@@ -431,25 +497,105 @@ void EvaluateForces(deque<t_particle > *particles, double particleMC, double elF
 }
 
 // ################################################################################
+// INFO CODE
+// ################################################################################
+
+bool PrintInfo()
+{
+    cout << "##############################################" << endl;
+    cout << "######## Starting the PIC simulation #########" << endl;
+    cout << "##############################################" << endl;
+    cout << endl;
+    cout << "##############################################" << endl;
+    cout << "################ Constants ###################" << endl;
+    cout << "##############################################" << endl;
+    cout << "       eps0  = " << m_eps_0    << endl;
+    cout << "       kb    = " << m_boltzman << endl;
+    cout << "       c     =" << m_charge   << endl;
+    cout << "       Bx    = " << m_magB[0] << endl;
+    cout << "       By    = " << m_magB[1] << endl;
+    cout << "       Bz    = " << m_magB[2] << endl;
+    cout << "       Bsize = " << m_magBSize << endl;
+    cout << endl;
+    cout << "##############################################" << endl;
+    cout << "################ Electrons ###################" << endl;
+    cout << "##############################################" << endl;
+    cout << "       m     = " << m_elMass   << endl;
+    cout << "       c/m   = " << m_elMC     << endl;
+    cout << "       T     = " << m_elTemp   << endl;
+    cout << "       v     = " << m_elVel    << endl;
+    cout << "       omega = " << m_elOmega  << endl;
+    cout << "       rl    = " << m_elLarm   << endl;
+    cout << endl;
+    cout << "##############################################" << endl;
+    cout << "################# Ions #######################" << endl;
+    cout << "##############################################" << endl;
+    cout << "        m     = " << m_ionMass  << endl;
+    cout << "        c/m   = " << m_ionMC    << endl;
+    cout << "        T     = " << m_ionTemp  << endl;
+    cout << "        v     = " << m_ionVel   << endl;
+    cout << "        omega = " << m_ionOmega << endl;
+    cout << "        rl    = " << m_ionLarm  << endl;
+    cout << endl;
+    cout << "##############################################" << endl;
+    cout << "################ Parameters ##################" << endl;
+    cout << "##############################################" << endl;
+    cout << "        error = " << m_error            << endl;
+    cout << "         x    = " << m_widthX           << endl;
+    cout << "        Nx    = " << m_sizeX            << endl;
+    cout << "        dx    = " << dx                 << endl;
+    cout << "         y    = " << m_widthY           << endl;
+    cout << "        Ny    = " << m_sizeY            << endl;
+    cout << "        dy    = " << dy                 << endl;
+    cout << "        dt    = " << dt                 << endl;
+    cout << "        n0    = " << m_density          << endl;
+    cout << "        N     = " << m_nParticles       << endl;
+    cout << "        NReal = " << m_nMacroParticles  << endl;
+    cout << "        Ndiff = " << m_multStats        << endl;
+    cout << "        STEP  = " << m_nIterations      << endl;
+    cout << "        step  = " << m_nIterationsEmpty << endl;
+    cout << "##############################################" << endl;
+    cout << endl;
+    cout << "Iterations needed: " << endl;
+    cout << "    electron to traverse: " << m_widthX  / (m_elVel * dt) << endl;
+    cout << "    electron to rotate:   " << 1 / (m_elOmega * dt) << endl;
+    cout << "    ion to traverse: " << m_widthX  / (m_ionVel * dt) << endl;
+    cout << "    ion to rotate:   " << 1 / (m_ionOmega * dt) << endl;
+    cout << endl;
+
+    char proceed;
+    while (proceed != 'y' && proceed != 'n')
+    {
+        cout << "Are the input parameters correct? (y/n): ";
+        cin >> proceed;
+    }
+    if (proceed == 'y')
+        return true;
+    else
+        return false;
+}
+
+// ################################################################################
 // MAIN CODE
 // ################################################################################
 
 int main()
 {
-    cout << "##############################################" << endl;
-    cout << "######## Starting the PIC simulation #########" << endl;
-    cout << "dt = " << dt << endl;
-    cout << "x = " << m_widthX << ", dx = " << dx << endl;
-    cout << "y = " << m_widthY << ", dy = " << dy << endl;
-    cout << "Te = " << m_elTemp << ", ne = " << m_density << endl;
-    cout << "Ve = " << m_elVel << ", B = " << VectorPow((double*) m_magB, 0.5) << endl;
-    cout << "##############################################" << endl;
+    //
+    // Print constants and info
+    if (!PrintInfo())
+        return -1;
 
+    //
+    // Randomize input
     srand(time(NULL));
 
+    //
+    // Define all needed fields
     double rho[m_sizeX][m_sizeY];
     double phi[m_sizeX][m_sizeY];
     double elField[m_sizeX][m_sizeY][3];
+    t_magField magElConst, magIonConst;
 
     //
     // Initialize fields
@@ -457,128 +603,26 @@ int main()
     InitField(phi);
 
     //
+    // Initialize magnetic field constants for forces evaluation
+    InitMagField(&magElConst);
+    InitMagField(&magIonConst);
+
+    //
     // Initialize geometry
     InitGeometry();
 
-    // -------------------------
-
     //
-    // rotation due to mag field
-    double magBMagnitudeSquared = VectorPow((double*) m_magB, 1);
-    double magOmegaEl  = sqrt(magBMagnitudeSquared) * m_elMC  / (2.0 * M_PI);
-    double magOmegaIon = sqrt(magBMagnitudeSquared) * m_ionMC / (2.0 * M_PI);
-    double magLarmorEl  = (2.0 * M_PI)*m_elVel/(m_elMC*sqrt(magBMagnitudeSquared));
-    double magLarmorIon = (2.0 * M_PI)*m_ionVel/(m_ionMC*sqrt(magBMagnitudeSquared));
-    cout << "Electrons: omega: " << magOmegaEl << ", larmor radius: " << magLarmorEl << endl;
-    cout << "Ions: omega: " << magOmegaIon << ", larmor radius: " << magLarmorIon << endl;
-
-    t_magField magElConst, magIonConst;
-
-    // tan for mag field electrons
-    if (magBMagnitudeSquared != 0)
-    {
-        magElConst.tan[0] = m_magB[0] * tan(m_elMC * magBMagnitudeSquared * dt / 2.0) / magBMagnitudeSquared;
-        magElConst.tan[1] = m_magB[1] * tan(m_elMC * magBMagnitudeSquared * dt / 2.0) / magBMagnitudeSquared;
-        magElConst.tan[2] = 0;
-    }
-    else
-    {
-        magElConst.tan[0] = 0;
-        magElConst.tan[1] = 0;
-        magElConst.tan[2] = 0;
-    }
-    double magTanElMagnitudeSquared = VectorPow(magElConst.tan,1);
-
-    // sin for mag field electrons
-    magElConst.sin[0] = 2 * magElConst.tan[0] / (1 + magTanElMagnitudeSquared);
-    magElConst.sin[1] = 2 * magElConst.tan[1] / (1 + magTanElMagnitudeSquared);
-    magElConst.sin[2] = 0;
-
-    // tan for mag field ions
-    if (magBMagnitudeSquared != 0)
-    {
-        magIonConst.tan[0] = m_magB[0] * tan(m_ionMC * magBMagnitudeSquared * dt / 2.0) / magBMagnitudeSquared;
-        magIonConst.tan[1] = m_magB[1] * tan(m_ionMC * magBMagnitudeSquared * dt / 2.0) / magBMagnitudeSquared;
-        magIonConst.tan[2] = 0;
-    }
-    else
-    {
-        magIonConst.tan[0] = 0;
-        magIonConst.tan[1] = 0;
-        magIonConst.tan[2] = 0;
-    }
-    double magTanIonMagnitudeSquared = VectorPow(magIonConst.tan,1);
-
-    // sin for mag field ions
-    magIonConst.sin[0] = 2 * magIonConst.tan[0] / (1 + magTanIonMagnitudeSquared);
-    magIonConst.sin[1] = 2 * magIonConst.tan[1] / (1 + magTanIonMagnitudeSquared);
-    magIonConst.sin[2] = 0;
-    // -------------------------
-
+    // Generate particle sources and monitored particles
     deque<t_particle > electronSource;
+    GenerateParticles(&electronSource, m_elVel);
     deque<t_particle > electrons;
-    t_particle electron;
-    double electronCharge = (-1) * m_charge * m_multStats;
 
     deque<t_particle > ionSource;
+    GenerateParticles(&ionSource, m_ionVel);
     deque<t_particle > ions;
-    t_particle ion;
+
+    double electronCharge = (-1) * m_charge * m_multStats;
     double ionCharge = m_charge * m_multStats;
-
-    //
-    // Generate m_nParticles random particles
-
-    double U1, U2, U3, U4;
-    for (uint particle = 0; particle < m_nMacroParticles; particle++)
-    {
-        //
-        // generation of electrons
-
-        // position
-        electron.pos[0] = m_widthX*((double) rand())/(RAND_MAX);
-        electron.pos[1] = m_widthY*((double) rand())/(RAND_MAX);
-        electron.node[0] = (uint) (electron.pos[0]/dx);
-        electron.node[1] = (uint) (electron.pos[1]/dy);
-
-//        // velocity
-        U1 = ((double) rand())/RAND_MAX;
-        U2 = ((double) rand())/RAND_MAX;
-        U3 = ((double) rand())/RAND_MAX;
-        U4 = ((double) rand())/RAND_MAX;
-        electron.vel[0] = ((m_elVel/sqrt(3))*sqrt(-2 * log(U1)) * cos(2 * M_PI * U2));
-        electron.vel[1] = ((m_elVel/sqrt(3))*sqrt(-2 * log(U1)) * sin(2 * M_PI * U2));
-        electron.vel[2] = ((m_elVel/sqrt(3))*sqrt(-2 * log(U3)) * cos(2 * M_PI * U4));
-//        electron.vel[0] = 0;
-//        electron.vel[1] = 0;
-//        electron.vel[2] = 0;
-
-        electronSource.push_back(electron);
-        // -------------------------
-
-        //
-        // generation of ions
-
-        // position
-        ion.pos[0] = m_widthX*((double) rand())/(RAND_MAX);
-        ion.pos[1] = m_widthY*((double) rand())/(RAND_MAX);
-        ion.node[0] = (uint) (ion.pos[0]/dx);
-        ion.node[1] = (uint) (ion.pos[1]/dy);
-
-        // velocity
-        U1 = ((double) rand())/RAND_MAX;
-        U2 = ((double) rand())/RAND_MAX;
-        U3 = ((double) rand())/RAND_MAX;
-        U4 = ((double) rand())/RAND_MAX;
-        ion.vel[0] = ((m_ionVel/sqrt(3)) * sqrt(-2 * log(U1)) * cos(2 * M_PI * U2));
-        ion.vel[1] = ((m_ionVel/sqrt(3)) * sqrt(-2 * log(U1)) * sin(2 * M_PI * U2));
-        ion.vel[2] = ((m_ionVel/sqrt(3)) * sqrt(-2 * log(U3)) * cos(2 * M_PI * U4));
-//        ion.vel[0] = 0;
-//        ion.vel[1] = 0;
-//        ion.vel[2] = 0;
-
-        ionSource.push_back(ion);
-        // -------------------------
-    }
 
     //
     // Main part of the code
